8617ed0ca63d511240f63d55bbe732add84879a2
sangtawee <sangtawee.techakittikajorn@ktb.co.th>
2020-02-11 17:19:00 +0700

    Updates

---
+++ precache-manifest.5c3b5e6f8cf6763d461d119ed6fce5fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de090a843a50711489e9f3209c7a585",
+    "url": "/demo-git-page/index.html"
+  },
+  {
+    "revision": "d680df9dbf37acd1533f",
+    "url": "/demo-git-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c888c254506a1e695e37",
+    "url": "/demo-git-page/static/js/2.feacd80b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/demo-git-page/static/js/2.feacd80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d680df9dbf37acd1533f",
+    "url": "/demo-git-page/static/js/main.9c4a4836.chunk.js"
+  },
+  {
+    "revision": "30b5a44529d6b745c51a",
+    "url": "/demo-git-page/static/js/runtime-main.93371954.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-git-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
